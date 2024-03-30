353c6a49c63aae9c26122c112428d820bb5bba9a
Adem Balaban <woodoo9576@hotmail.com>
2024-03-30 15:54:01 +0300

    ADDing context

---
+++ Mc2.CrudTest.DataBase/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mc2.CrudTest.Domain.Abstract;
+using Mc2.CrudTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.DataBase
+{
+    internal class ApplicationContext : DbContext,IApplicationContext
+    {
+        private DbSet<Customer> _customers;
+
+        public DbSet<Customer> Customers
+        {
+            get => _customers;
+            set => _customers = value;
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
