25b8553028c438cca15b63bcdd97dfd21b0c2893
Adem Balaban <woodoo9576@hotmail.com>
2024-03-30 19:40:20 +0300

    validation by Data annotations

--- Mc2.CrusTest.Domain/Common/Entities/BaseEntity.cs
+++ Mc2.CrusTest.Domain/Common/Entities/BaseEntity.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mc2.CrudTest.Domain.Common.Entities
 {
@@ -19,6 +21,7 @@ namespace Mc2.CrudTest.Domain.Common.Entities
 
     public interface IBaseEntity
     {
+        [Key]
         int Id { get; set; }
     }
 }
--- Mc2.CrusTest.Domain/Entities/Customer.cs
+++ Mc2.CrusTest.Domain/Entities/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,18 @@ namespace Mc2.CrudTest.Domain.Entities
 {
     public class Customer : BaseEntity
     {
+        [Required,MinLength(2,ErrorMessage = "Please Enter FirstName"),MaxLength(55,ErrorMessage = "FirstName cannot exceed 55 characters")]
         public string FirstName { get; set; }
+        [Required,MinLength(2,ErrorMessage = "Please Enter LastName"),MaxLength(55,ErrorMessage = "LastName cannot exceed 55 characters")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Date Of Birth is Required")]
+        [DataType(DataType.Date,ErrorMessage = "Please Enter valid date Format")]
         public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Phone Number is Required")]
         public long PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Email is Required"),DataType(DataType.EmailAddress,ErrorMessage = "Please Enter Valid E-Mail address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Bank Account Number is Required"),Range(1000000000,9999999999,ErrorMessage = "Please Enter Valid Bank Account Number")]
         public long BankAccountNumber { get; set; }
 
     }
