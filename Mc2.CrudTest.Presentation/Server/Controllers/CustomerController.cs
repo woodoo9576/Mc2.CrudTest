b11ebd987e8e516b93e7931ac00cbb0e235b514d
Adem Balaban <woodoo9576@hotmail.com>
2024-03-30 15:59:45 +0300

    Create Dependencies

---
+++ Mc2.CrudTest.Presentation/Server/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers
+{
+    public class CustomerController : ControllerBase
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
